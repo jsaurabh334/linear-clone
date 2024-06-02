11b006b44aff7276c4756cc3254503e301453b3e
jsaurabh334 <jsaurabh334@gmail.com>
2024-06-02 21:54:21 +0530

    final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Linear - Prajil",
+  title: "Trading arena",
   description: "Linear clone developed by Prajil",
 };
 

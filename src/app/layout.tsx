9f4bf90892e3dd6516a3da009bad28c34accea62
jsaurabh334 <jsaurabh334@gmail.com>
2024-06-03 15:15:54 +0530

    final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Trading arena",
-  description: "Linear clone developed by Prajil",
+  description: "Linear clone developed by bitcode",
 };
 
 export default function RootLayout({
